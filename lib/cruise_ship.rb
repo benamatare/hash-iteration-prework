7ead98ce5a5dd2e9ac51b463d5a2482d41409aba
Ben Matare <ben.a.matare@gmail.com>
2018-01-23 05:03:47 +0000

    automatically backed up by learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -1,11 +1,15 @@
-# passengers = {
-# suite_a: "Amanda Presley",
-# suite_b: "Seymour Hoffman",
-# suite_c: "Alfred Tennyson",
-# suite_d: "Charlie Chaplin",
-# suite_e: "Crumpet the Elf"
-# }
+ passengers = {
+ suite_a: "Amanda Presley",
+ suite_b: "Seymour Hoffman",
+ suite_c: "Alfred Tennyson",
+ suite_d: "Charlie Chaplin",
+ suite_e: "Crumpet the Elf"
+ }
 
-def select_winner(passengers)
-
-end
+ def select_winner(passengers)
+   passengers.each do |suite, passenger|
+     if suite == :suite_a && passenger.start_with?("A")
+       puts passenger
+       end
+     end
+ end
